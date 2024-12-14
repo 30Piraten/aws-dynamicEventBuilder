f72d435c524c76477ed9af70d22c975f436a33a0
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-14 19:50:41 +0100

    Added api-gateway connection to lambda function

---
+++ modules/api-gateway/api.tf
@@ -0,0 +1,88 @@
+resource "aws_api_gateway_account" "api" {
+    cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
+
+data "aws_iam_policy_document" "role" {
+  statement {
+    effect = "Allow"
+  
+    principals {
+        type = "Service"
+        identifiers = ["apigateway.amazonaws.com"]
+    }
+    actions = ["sts.AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "cloudwatch" {
+    name = "api_gateway_cloudwatch_global"
+    assume_role_policy = data.aws_iam_policy_document.role.json
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  statement {
+    effect = "Allow"
+  
+    actions = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:FilterLogEvents",
+    ]
+    // This needs to be modified and restricted
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "default"
+  role = aws_iam_role.cloudwatch.id
+  policy = data.aws_iam_policy_document.cloudwatch.json
+}
+
+// Connecting API Gateway to Lambda
+resource "aws_api_gateway_rest_api" "api" {
+  name = ""
+  description = "API Gateway for Lambda"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_rest_api.api.root_resource_id
+  path_part = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  # uri = aws_lambda_function.lambda_func.invoke_arn
+  uri = module.lambda.lambda_func.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id = "AllowPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  # function_name = aws_lambda_function.lambda_func.function_name
+  function_name = module.lambda_func.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "{aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
