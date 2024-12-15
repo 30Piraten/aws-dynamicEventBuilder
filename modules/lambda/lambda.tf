0902792b18b19f873a58be6f2b4f207b1be59266
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-15 06:14:19 +0100

    Updated variable & output. Changed source_arn

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,40 +1,48 @@
 // Lambda IAM Role Definition
 
 resource "aws_iam_role" "lambda_exec" {
-    name = "lambda_execution_role"
+  name = "lambda_execution_role"
 
-    assume_role_policy = jsondecode({
-        Version = "",
-        Statement = [
-            {
-                Effect = "Allow",
-                Principal = { Service = "lambda.amazonaws.com"},
-                Action = "sts:AssumeRole"
-            }
-        ]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2024-12-14",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "lambda.amazonaws.com" },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "lambda_logging" {
-    name = "lambda_logging_policy"
-    role = aws_iam_role.lambda_exec.id
+  name = "lambda_logging_policy"
+  role = aws_iam_role.lambda_exec.id
 
-    policy = jsondecode({
-        Version = "",
-        Statement = [
-            {
-                Effect = "Allow",
-                Action = "logs:PutLogEvents",
-                Resource = "arn:aws:logs:*:*:*"
-            }
-        ]
-    })
+  policy = jsonencode({
+    Version = "2024-12-14",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "logs:PutLogEvents",
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_function" "lambda_func" {
-    function_name = ""
-    role = aws_iam_role.lambda_exec.arn 
-    runtime = "go1.x" // python instead
-    handler = "main"
-    filename = "lambda_function_payload.zip"
+  function_name = "lambdafunc2"
+  role          = aws_iam_role.lambda_exec.arn
+  runtime       = "go1.x" // python instead
+  handler       = "main"
+  filename      = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_func.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = var.source_arn
 }
