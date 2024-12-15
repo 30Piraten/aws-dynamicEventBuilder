0fc141f16a3ed96f3822b95f37a610196923c29a
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-15 07:11:45 +0100

    TODO: aws_iam_policy_document needs to be ammended

--- modules/api-gateway/api.tf
+++ modules/api-gateway/api.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "cloudwatch" {
       "logs:GetLogEvents",
       "logs:FilterLogEvents",
     ]
-    // This needs to be modified and restricted
+    // TODO: This needs to be modified and restricted
     resources = ["*"]
   }
 }
