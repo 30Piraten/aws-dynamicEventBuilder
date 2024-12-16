5d814206d885c488efd18c479591a931610850b9
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-16 12:44:02 +0100

    Corrected comment error

--- lambda-functions/provisionenv/proenv.go
+++ lambda-functions/provisionenv/proenv.go
@@ -1,29 +1,30 @@
 package provisionenv
 
 import (
-  "context"
-  "fmt"
-  "github.com/aws/aws-lambda-go/lambda"
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/ec2"
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func HandleRequest(ctx context.Context) (string, error) {
-  sess := session.Must(session.NewSession())
-  svc := ec2.New(sess)
+	sess := session.Must(session.NewSession())
+	svc := ec2.New(sess)
 
-  input := &ec2.RunInstancesInput{
-    ImageId:      aws.String("ami-0c55b159cbfafe1f0"),  # Replace with a valid AMI ID
-    InstanceType: aws.String("t2.micro"),
-    MinCount:     aws.Int64(1),
-    MaxCount:     aws.Int64(1),
-  }
+	input := &ec2.RunInstancesInput{
+		ImageId: aws.String("ami-0c55b159cbfafe1f0"),
+		// Replace with a valid AMI ID
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	}
 
-  _, err := svc.RunInstances(input)
-  if err != nil {
-    return "", fmt.Errorf("could not create instance: %v", err)
-  }
+	_, err := svc.RunInstances(input)
+	if err != nil {
+		return "", fmt.Errorf("could not create instance: %v", err)
+	}
 
-  return "Instance created successfully", nil
+	return "Instance created successfully", nil
 }
