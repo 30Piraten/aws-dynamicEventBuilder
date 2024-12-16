0bd355dc119ed585da37f7017c6fc705300a31c2
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-16 06:42:56 +0100

    Added connection to api_gateway. Provisioned lambda function payload

--- modules/api-gateway/outputs.tf
+++ modules/api-gateway/outputs.tf
@@ -1,7 +1,9 @@
-output "api_gateway_rest_api" {
-  value = aws_api_gateway_rest_api.api.id
-}
+// For REST API Gateway
+# output "api_gateway_rest_api" {
+#   value = aws_api_gateway_rest_api.api.id
+# }
 
-output "api_gateway_execution_arn" {
-  value = "${aws_api_gateway_rest_api.api.execution_arn}/*"
-}
+# // For source_arn
+# output "execution_arn" {
+#   value = "${aws_api_gateway_rest_api.api.execution_arn}/*"
+# }
--- modules/api-gateway/variable.tf
+++ modules/api-gateway/variable.tf
@@ -1,3 +1,7 @@
-variable "uri" {
-  type = string
-}
+# variable "uri" {
+#   type = string
+# }
+
+# variable "api_gateway_rest_api_name" {
+#   type = string 
+# }
