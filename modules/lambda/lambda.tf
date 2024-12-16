760300121e9a23c2408bc2b67c41a794c68bfcdc
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-16 12:44:51 +0100

    Updated lambda config with necessary permission

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,10 +1,9 @@
 // Lambda IAM Role Definition
-
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2024-12-14",
+    Version = "2012-10-17",
     Statement = [
       {
         Effect    = "Allow",
@@ -20,11 +19,15 @@ resource "aws_iam_role_policy" "lambda_logging" {
   role = aws_iam_role.lambda_exec.id
 
   policy = jsonencode({
-    Version = "2024-12-14",
+    Version = "2012-10-17",
     Statement = [
       {
         Effect   = "Allow",
-        Action   = "logs:PutLogEvents",
+        Action   = [
+          "logs:PutLogEvents",
+          "logs:CreateLogsStream",
+          "logs:PutLogEvents"
+        ]
         Resource = "arn:aws:logs:*:*:*"
       }
     ]
@@ -38,47 +41,58 @@ resource "null_resource" "build_lambdas" {
   }
 }
 
-// TODO: USE DYNAMIC SETTINGS
-// Lambda function resource configuration
-// for cleanupenv and provisionenv
+// Lambda variables for dynamic settings
+locals {
+  lambda_functions = {
+    cleanupenv = {
+      name = "cleanup_lambda"
+      filepath = "${path.root}/lambda-functions/cleanupenv/lambda_function_payload.zip"
+    }
+    provisionenv = {
+      name = "proenv_lambda"
+      filepath = "${path.root}/lambda-functions/provisionenv/lambda_function_payload.zip"
+    }
+  }
+}
+
+// Lambda function resource configurations
 resource "aws_lambda_function" "cleanupenv" {
-  function_name = "cleanup_lambda"
+  function_name = local.lambda_functions["cleanupenv"].name 
   role          = aws_iam_role.lambda_exec.arn
-  runtime       = "go1.x" // python instead
+  runtime       = "go1.x"
   handler       = "main"
-  filename      = "lambda_function_payload.zip"
+  filename      = local.lambda_functions["cleanupenv"].filepath
   depends_on    = [null_resource.build_lambdas]
 }
 
 resource "aws_lambda_function" "provisionenv" {
-  function_name = "proenv_lambda"
-  role          = aws_iam_role.lambda_exec
+  function_name = local.lambda_functions["provisionenv"].name
+  role          = aws_iam_role.lambda_exec.arn
   runtime       = "go1.x"
   handler       = "main"
-  filename      = "${path.root}/lambda_functions/provisionenv/proenv.go"
+  filename      = local.lambda_functions["provisionenv"].filepath
   depends_on    = [null_resource.build_lambdas]
 }
 
-// TODO: USE DYNAMIC SETTINGS 
 // Lambda permission for API Gateway
 // this includes permission for both cleanup and proenv
 resource "aws_lambda_permission" "api_gateway_cleanupenv" {
-  statement_id  = "AllowPIGatewayInvoke"
+  statement_id  = "AllowPIGatewayInvokeCleanup"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cleanupenv.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = aws_api_gateway_rest_api.api.execution_arn
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*"
 }
 
 resource "aws_lambda_permission" "api_gateway_proenv" {
-  statement_id  = "AllowPIGatewayInvoke"
+  statement_id  = "AllowPIGatewayInvokeProenv"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.provisionenv.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = aws_api_gateway_rest_api.api.execution_arn
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*"
 }
 
-// Connecting API Gateway to Lambda
+// API Gateway Configuration
 resource "aws_api_gateway_rest_api" "api" {
   name        = "gateway_api"
   description = "API Gateway for Lambda"
@@ -101,12 +115,22 @@ resource "aws_api_gateway_method" "proxy_method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "cleanupenv" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.proxy_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_func.invoke_arn
+  uri                     = aws_lambda_function.cleanupenv.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "provisionenv" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.provisionenv.invoke_arn
 }
