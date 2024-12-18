72171166a214d39ff819152cfbebd94083a593a0
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-18 06:58:37 +0100

    Removed lagging comments

--- modules/api-gateway/api.tf
+++ modules/api-gateway/api.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "role" {
       type        = "Service"
       identifiers = ["apigateway.amazonaws.com"]
     }
-    actions = ["sts.AssumeRole"]
+    actions = ["sts:AssumeRole"]
   }
   version = "2012-10-17"
 }
@@ -43,37 +43,3 @@ resource "aws_iam_role_policy" "cloudwatch" {
   role   = aws_iam_role.cloudwatch.id
   policy = data.aws_iam_policy_document.cloudwatch.json
 }
-
-# // Connecting API Gateway to Lambda
-# resource "aws_api_gateway_rest_api" "api" {
-#   name        = "gateway_api"
-#   description = "API Gateway for Lambda"
-
-#   endpoint_configuration {
-#     types = ["REGIONAL"]
-#   }
-# }
-
-# resource "aws_api_gateway_resource" "proxy" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-#   path_part   = "{proxy+}"
-# }
-
-# resource "aws_api_gateway_method" "proxy_method" {
-#   rest_api_id   = aws_api_gateway_rest_api.api.id
-#   resource_id   = aws_api_gateway_resource.proxy.id
-#   http_method   = "ANY"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "lambda" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   resource_id = aws_api_gateway_resource.proxy.id
-#   http_method = aws_api_gateway_method.proxy_method.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   # uri = aws_lambda_function.lambda_func.invoke_arn
-#   uri = var.uri
-# }
