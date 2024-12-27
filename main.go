0fc84de7f7e149f45b673c2639db1bfa0a2217b6
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-27 21:50:53 +0100

    Imported cleanupenv

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	cleanup "github.com/30Piraten/aws_dynamicEventBuilder/lambda-functions/cleanupenv"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(cleanup.HandleCleanUpRequest)
 }
