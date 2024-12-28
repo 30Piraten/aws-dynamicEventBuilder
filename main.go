be5bdb1bd2a6d247a1c6c63433e4c5b09d15140a
30Piraten <victorehikioyaraeva@outlook.com>
2024-12-28 14:46:02 +0100

    Imported cleanup & proenv func

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	cleanup "github.com/30Piraten/aws_dynamicEventBuilder/lambda-functions/cleanupenv"
+	cleanup "github.com/30Piraten/aws-dynamicEventBuilder/lambda-functions/cleanupenv"
+	proenv "github.com/30Piraten/aws-dynamicEventBuilder/lambda-functions/provisionenv"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	lambda.Start(cleanup.HandleCleanUpRequest)
+	lambda.Start(cleanup.HandleCleanupRequest)
+	lambda.Start(proenv.HandleProvisionRequest)
+	// lambda.Start(proenv.HandleProvisionRequest)
 }
